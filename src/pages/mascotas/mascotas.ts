71b3ab44e32a811b214384890ee812b1b80c3c8c
KaroCas <carito.castro.a@gmail.com>
2018-07-03 21:48:59 -0400

    likes

--- src/pages/mascotas/mascotas.ts
+++ src/pages/mascotas/mascotas.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+
 /**
  * Generated class for the MascotasPage page.
  *
@@ -14,12 +15,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'mascotas.html',
 })
 export class MascotasPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+ 
+  public tap: number = 0;
+  constructor(public navCtrl: NavController, public navParams: NavParams ) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad MascotasPage');
-  }
-
+  tapEvent(e) {
+    this.tap++
+}
+  
 }
